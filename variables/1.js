68f089536777610c4835a4483d9df03297fd058d
Souradeep Das <donnyforofficial@gmail.com>
2023-09-09 13:35:30 +0530

    added new files

--- variables/1.js
+++ variables/1.js
@@ -9,3 +9,6 @@ console.log(firstName);
 firstname = "Abhi";
 console.log(firstname);
 console.log(firstName);
+
+let country ="INDIA";
+console.log(country)
---
+++ variables/string indexing.js
@@ -0,0 +1,12 @@
+//STRING INDEXING
+
+let firstName='SOURADEEP';
+console.log(firstName[4])
+
+//LENGTH OF STRING
+let p=firstName.length
+console.log(p)
+
+//PRINTING LAST INDEX
+let last_index = firstName[firstName.length-1]
+console.log(last_index)
